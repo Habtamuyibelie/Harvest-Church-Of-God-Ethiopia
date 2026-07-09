880b2a49e59ff144e83ccee7adb61908f0e8f905
Habtamuyibelie <135222544+Habtamuyibelie@users.noreply.github.com>
2026-07-09 09:31:38 +0300

    Update social-links.ts
    link update

--- lib/social-links.ts
+++ lib/social-links.ts
@@ -7,7 +7,7 @@ import { Facebook, Youtube, Send, Instagram, Music2, Twitter } from "lucide-reac
 export const socialLinks = [
   { label: "Facebook", href: "https://www.facebook.com/HarvestChurchOfGodEthiopia", Icon: Facebook },
   { label: "YouTube",  href: "https://www.youtube.com/@harvestchurchofgodethiopia", Icon: Youtube },
-  { label: "Telegram", href: "https://web.telegram.org/k/#-1370621699",         Icon: Send },
+  { label: "Telegram", href: "https://t.me/HarvestChurchofGod",         Icon: Send },
   { label: "Instagram",href: "https://instagram.com/harvestchurchethiopia",Icon: Instagram },
   { label: "TikTok",   href: "https://tiktok.com/@harvestchurchethiopia",  Icon: Music2 },
     { label: "Twitter",   href: "https://x.com/HARVESTCOG?s=20",  Icon: Twitter },
